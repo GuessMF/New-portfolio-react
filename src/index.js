3d73191d82eaeacc974c2510b065aa727b186a7d
Sergey <Segas95@yandex.ru>
2023-05-18 15:13:28 +0300

    cdcd

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.scss";
 import App from "./components/App/App";
-import {BrowserRouter as Router} from "react-router-dom";
+import {HashRouter as Router} from "react-router-dom";
 //import {createMemoryHistory} from "history";
 //import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //const history = createMemoryHistory();
 root.render(
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <App />
   </Router>
   // <React.StrictMode>
