8876bd9487c4034a099b967ccbf6fe2995528978
Sergey <Segas95@yandex.ru>
2024-02-05 12:55:04 +0300

    22

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React, {Suspense} from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.scss";
 import App from "./components/App/App";
 import {HashRouter as Router} from "react-router-dom";
-import {Audio} from "react-loader-spinner";
+
 //import {createMemoryHistory} from "history";
 //import reportWebVitals from './reportWebVitals';
 
@@ -13,21 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <Router basename={process.env.PUBLIC_URL}>
-    <Suspense
-      fallback={
-        <Audio
-          height="80"
-          width="80"
-          radius="9"
-          color="green"
-          ariaLabel="three-dots-loading"
-          wrapperStyle
-          wrapperClass
-        />
-      }
-    >
-      <App />
-    </Suspense>
+    <App />
   </Router>
 );
 //reportWebVitals();
